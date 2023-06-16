ffb272d2d1d4116bc3d74c2d2216a4587115ab3b
lucasbertolinogb <102763228+lucasbertolinogb@users.noreply.github.com>
2023-06-16 16:05:35 -0300

    Update index.js

--- src/login/index.js
+++ src/login/index.js
@@ -1,15 +1,36 @@
-import { Text , TouchableOpacity, View} from "react-native";
+import { TouchableOpacity, View , TextInput, SafeAreaView} from "react-native";
 import styles from "../components/style";
 import { Link } from '@react-navigation/native';
+import React, { useState } from 'react';
 
-export default function cadastro () {
+export default function login () {
+    
+    const [text, onChangeText] = React.useState('Email genérico');
+    const [number, onChangeNumber] = React.useState('');
+
     return(
         <View style={styles.container}>
-            <Text>Bem vindo(a) a taverna do heroi </Text>
-            <Text>Logue em sua conta para acessa-las</Text>
+            <SafeAreaView>
+                    <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                     />
+                     <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeNumber}
+                    value={number}
+                    placeholder="Senha genérica"
+                    keyboardType="numeric"
+                    />
+            </SafeAreaView>
 
             <TouchableOpacity style={styles.button}>
-                <Link to={{screen: "login"}}>Email</Link> 
+                <Link to={{screen: "MoviesPage"}}>Logar</Link> 
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.button}>
+                <Link to={{screen: "RecuperarSenha"}}>RECUPERAR SENHA</Link> 
             </TouchableOpacity>
 
 
